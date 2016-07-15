4157b93e1865294ea41dd515e4a04d7ea1bc0085
Jack Brown <brown285@umn.edu>
2016-07-14 23:08:06 -0500

    Add user security
    - Added link to edit user in header
    - Added name and pfs_number to sign-up page
    - require name and pfs_number now
    - locked down appropriate pages to admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_events
+  validates :name , :pfs_number, :email, :presence => true
 end
