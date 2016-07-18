15d8f985141a7a7a81c85c547dc783b17aa1dc02
Jack Brown <brown285@umn.edu>
2016-07-18 16:17:21 -0500

    Add ActionMailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'hidden-crag-47757.herokuapp.com',
+      :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
