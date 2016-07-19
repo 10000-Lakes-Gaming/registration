e7b90b868a75e9d47541fb8341d2c77f349f7542
Jack Brown <brown285@umn.edu>
2016-07-19 14:08:13 -0500

    Allow to raise errors on email send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'pfs-registration.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   ActionMailer::Base.smtp_settings = {
