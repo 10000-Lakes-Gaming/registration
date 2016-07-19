ab8b04a347bb9efef6a9c7293da6284c1b795439
Jack Brown <brown285@umn.edu>
2016-07-19 14:04:27 -0500

    Add configuration for gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {:host => 'pfs-registration.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
   ActionMailer::Base.smtp_settings = {
       :address => 'smtp.gmail.com',
