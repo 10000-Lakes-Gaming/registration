1d4188be757d80ef39aa892db39f0b6e8850e8e4
Jack Brown <brown285@umn.edu>
2016-08-12 09:01:57 -0500

    Change email_address to email (which is correct)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -95,7 +95,7 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:name, :email_address, :pfs_number, :admin)
+    params.require(:user).permit(:name, :email, :pfs_number, :admin)
   end
 
 end
