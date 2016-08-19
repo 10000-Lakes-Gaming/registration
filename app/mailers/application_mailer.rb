7b70e52c5c402eb3484de194cdcf6398f1ff0763
Jack Brown <brown285@umn.edu>
2016-08-19 12:23:02 -0500

    add Precendence bulk

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   add_template_helper(EmailHelper)
 
-  default from: "mn.pfs.reg@gmail.com"
+  default from: "mn.pfs.reg@gmail.com", "Precedence" => "bulk"
   default to: "mn.pfs.reg@gmail.com"
   # layout 'mailer'
 end
