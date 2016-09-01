e76e5a958b69509abb897f0b3011195f2873908c
Jack Brown <brown285@umn.edu>
2016-09-01 13:58:31 -0500

    Trying to remove duplicate scenarios

--- app/controllers/gm_list_controller.rb
+++ app/controllers/gm_list_controller.rb
@@ -7,8 +7,16 @@ class GmListController < ApplicationController
     @event.user_events.each do |user_event|
       @game_masters = (@game_masters << user_event.game_masters).flatten!
     end
-
     @game_masters = @game_masters.sort { |a, b| a <=> b }
 
+    # now remove duplicate scenarios
+    previous_scenario
+    @game_masters.each do |gm|
+      if gm.table.scenario == previous_scenario
+        @game_masters - [gm]
+      else
+        previous_scenario = gm.table.scenario
+      end
+    end
   end
 end
