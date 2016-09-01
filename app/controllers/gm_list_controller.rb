ab27014ba5045b0e7a5fa289ce7709c9d7a8d92a
Jack Brown <brown285@umn.edu>
2016-09-01 13:40:52 -0500

    Add the controller. d'oh

---
+++ app/controllers/gm_list_controller.rb
@@ -0,0 +1,11 @@
+class GmListController < ApplicationController
+  before_action :get_event
+
+  def index
+    # need to collect in the stuff. Make it easier to consume
+    @game_masters = []
+    @event.user_events.each do |user_event|
+      @game_masters = (@game_masters << user_event.game_masters).flatten!
+    end
+  end
+end
