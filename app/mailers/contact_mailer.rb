24303d27280a1a42a9cf670277f005b19ca1f3e4
Jack Brown <brown285@umn.edu>
2016-09-13 14:12:38 -0500

    Slice the email list

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -21,8 +21,10 @@ class ContactMailer < ApplicationMailer
 
   def session_reminder(message, emails)
     @message = message
-    @emails = emails
-    mail(subject: @message.subject, bcc: @emails)
+    emails..each_slice(10) do |slice|
+      @emails = slice
+      mail(subject: @message.subject, bcc: @emails)
+    end
   end
 
   def payment_reminder(message, emails)
