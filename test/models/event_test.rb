4ce6e072652218f08c62752832e2700eb6e1d946
Jack Brown <brown285@umn.edu>
2017-03-30 11:34:16 -0500

    Add basic tests for events
    Includes checks for user events existing (but not tests on the events themselves)

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @my_event    = events(:my_event)
+    @other_event = events(:other)
+  end
+
+
+  test "Can load My Event!" do
+    assert @my_event.name == 'My Event'
+  end
+
+  test "Can load other event" do
+    assert @other_event
+  end
+
+  test "RSVPs are closed on my event" do
+    assert @my_event.closed?
+  end
+
+  test "RSVPs are OPEN on other event" do
+    assert_not @other_event.closed?
+  end
+
+  test "Events are not the same!" do
+    assert_not_equal @my_event, @other_event
+  end
+
+  test "The same event is  the same!" do
+    event = events(:my_event)
+    assert_equal @my_event, event
+  end
+
+  test "Events sort by name, and my event comes before other." do
+    assert (@my_event <=> @other_event) < 0
+  end
+
+  test "My event has 2 registrations, including the admin user's" do
+    admin_user_event = user_events(:admin_my_event)
+
+    assert_equal 2, @my_event.user_events.size
+    assert_includes @my_event.user_events, admin_user_event
+
+  end
+
 end
