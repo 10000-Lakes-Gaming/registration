5de7ee872f7b7f2efae36c4c0b92b0ebb13adea0
Jack Brown <brown285@umn.edu>
2017-04-12 11:14:21 -0500

    Simplify `<=>` method
    Use `Boolean.to_s` and sort from there.

--- app/models/table.rb
+++ app/models/table.rb
@@ -6,27 +6,15 @@ class Table < ActiveRecord::Base
   delegate :name, to: :scenario
   delegate :long_name, to: :scenario
   delegate :start, to: :session
+  delegate :end, to: :session
   validates :scenario_id, :session_id, :max_players, :gms_needed, :presence => true
   validates_numericality_of :gms_needed, :max_players, greater_than: 0
 
   def <=> (tab)
-    sort = 0
-    if tab.raffle == self.raffle
-      sort = 0
-    elsif tab.raffle
-      sort = -1
-    else
-      sort = 1
-    end
+    sort = self.raffle.to_s <=> tab.raffle.to_s
 
     if sort == 0
-      if tab.core == self.core
-        sort = 0
-      elsif tab.core
-        sort = -1
-      else
-        sort = 1
-      end
+      sort = self.core.to_s <=> tab.core.to_s
     end
 
     if sort == 0
