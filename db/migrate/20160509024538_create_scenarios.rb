481e0cc9b9b54b3f6156e9c47f3f410cea233d0a
Jack Brown <brown285@umn.edu>
2017-06-30 14:25:48 -0500

    Undo incorrect edit on `type` field`

--- db/migrate/20160509024538_create_scenarios.rb
+++ db/migrate/20160509024538_create_scenarios.rb
@@ -1,7 +1,7 @@
 class CreateScenarios < ActiveRecord::Migration
   def change
     create_table :scenarios do |t|
-      t.string :type_of
+      t.string :type
       t.integer :season
       t.integer :scenario_number
       t.string :name
@@ -14,3 +14,4 @@ class CreateScenarios < ActiveRecord::Migration
     end
   end
 end
+    
