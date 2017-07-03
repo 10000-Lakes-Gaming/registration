94a9e8847ebb3f24660eb3789a2468a86e34f9ad
Jack Brown <brown285@umn.edu>
2017-07-03 01:02:41 -0500

    Add initial work for adding table payments

--- app/controllers/table_payment_controller.rb
+++ app/controllers/table_payment_controller.rb
@@ -1,36 +1,32 @@
-class RegistrationPaymentController < ApplicationController
+class TablePaymentController < ApplicationController
 
-  before_action :get_event, :get_registration
-
-  def get_registration
-    @user_event = UserEvent.find(params[:user_event_id])
-  end
+  before_action :get_event, :get_registration_table,
 
   def new
+    @user_event = @registration_table.user_event
   end
+
+# localhost:3000//events/3/sessions/4/tables/7/registration_tables/5/table_payment
 
   def create
     # Amount in cents
-    @amount = @event.price * 100
-
-    customer = Stripe::Customer.create(
-        :email  => params[:stripeEmail],
-        :source => params[:stripeToken]
-    )
+    amount = @table.price * 100
+    token  = params[:stripeToken]
+    # customer = Stripe::Customer.create(
+    #     :email  => params[:stripeEmail],
+    #     :source => params[:stripeToken]
+    # )
 
     charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => @amount,
-        :description => "#{@user_event.user.name} payment for #{@event.name}",
+        :source      => token,
+        :amount      => amount,
+        :description => "#{@user_event.user.name} payment for #{@table.name}",
         :currency    => 'usd'
     )
 
-    logger.info "I have a charge #{charge}"
-    logger.info "The charge ID is #{charge.id} and it was in the amount of #{charge.amount / 100}"
-
-    @user_event.paid = true
+    @user_event.paid           = true
     @user_event.payment_amount = charge.amount # Will be in cents, not dollars!
-    @user_event.payment_id = charge.id
+    @user_event.payment_id     = charge.id
     @user_event.save!
 
   rescue Stripe::CardError => e
@@ -39,5 +35,25 @@ class RegistrationPaymentController < ApplicationController
     redirect_to new_registration_payment_path [@event, @user_event]
   end
 
+  private
+  def get_registration_table
+    @registration_table = RegistrationTable.find(params[:registration_table_id])
+  end
+
+  def get_table
+    @table = Table.find(params[:table_id])
+  end
+
+  def get_session
+    @session = @event.sessions.find(params[:session_id])
+  end
+
+  def get_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def get_registration_tables
+    @registration_tables = @table.registration_tables
+  end
 
 end
