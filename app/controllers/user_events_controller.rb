1eb2497af1d5ab7b25f0bee46237f38be6dea84e
Jack Brown <brown285@umn.edu>
2017-07-05 13:16:05 -0500

    Move `registration_payment` partial into logical location
    Plus trying to fix the fact that we cannot get the `user_event/show.html.erb` to render

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,11 +1,6 @@
 class UserEventsController < ApplicationController
   before_action :get_user_event, only: [:show, :edit, :update, :destroy]
   before_action :get_event, :get_users, :get_all_events
-  before_action :get_stripe_public_key, only: [:show]
-
-  def get_stripe_public_key
-    @public_key = ENV["PUBLISHABLE_KEY"]
-  end
 
   def get_user
     @user = current_user
