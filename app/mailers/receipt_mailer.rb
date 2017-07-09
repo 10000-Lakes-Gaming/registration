7fa6b44e174163200b01da12215cdcd81b025433
Jack Brown <brown285@umn.edu>
2017-07-09 16:47:53 -0500

    Add emails for receipts
    Not wired in yet,  but previews work
    Preview all emails at http://localhost:3000/rails/mailers/receipt_mailer

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -1,12 +1,25 @@
 class ReceiptMailer < ApplicationMailer
 
   default from: "mn.pfs.reg@gmail.com"
-  default subject: "Payment Received"
 
   def event_registration_payment_email(user_event)
     @user_event = user_event
-    @event = @user_event.event
-    @user = @user_event.user
-    mail(to: @user.email)
+    @event      = @user_event.event
+    @user       = @user_event.user
+
+    @subject = "Payment Received for #{@event.name}"
+    mail(to: @user.email, subject: @subject)
+  end
+
+  def table_registration_payment_email(registration_table)
+    @registration_table = registration_table
+    @user_event         = @registration_table.user_event
+    @event              = @user_event.event
+    @user               = @user_event.user
+    @table              = @registration_table.table
+
+
+    @subject = "Payment Received for #{@table.long_name} at #{@event.name}"
+    mail(to: @user.email, subject: @subject)
   end
 end
