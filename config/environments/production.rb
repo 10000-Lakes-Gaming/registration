776173886cc45966a1a8987494043118df8e8f5c
Jack Brown <brown285@umn.edu>
2017-07-11 10:13:07 -0500

    Clean up some duplicate settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {:host => 'pfs-registration.herokuapp.com'}
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
