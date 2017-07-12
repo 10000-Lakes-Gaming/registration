db1d77649adae6bb84f7e7c90bcfd21f733b26de
Jack Brown <brown285@umn.edu>
2017-07-12 14:49:50 -0500

    Change domain?

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
       :address => 'smtp.gmail.com',
       :port => 587,
-      :domain => 'gmail.com',
+      :domain => '10klakesgaming.org',
       :authentication => :plain,
       :user_name => ENV['GMAIL_SMTP_USERNAME'],
       :password => ENV['GMAIL_SMTP_PASSWORD'],
