c7f11bc97de78d10e6569846d640ac1b882bde6a
Jack Brown <brown285@umn.edu>
2017-07-11 21:47:38 -0500

    Add title and GM stars to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711190658) do
+ActiveRecord::Schema.define(version: 20170712021156) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -121,6 +121,9 @@ ActiveRecord::Schema.define(version: 20170711190658) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "forum_username"
+    t.integer  "gm_stars"
+    t.boolean  "venture_officer"
+    t.string   "title"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["pfs_number"], name: "index_users_on_pfs_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
