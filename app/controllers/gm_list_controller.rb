054b321a672b67cae897bf7a02286e3510c0a3e5
Jack Brown <brown285@umn.edu>
2017-07-12 23:21:58 -0500

    Add JSON of gm list.
    GM list also pads TBDs mow

--- app/controllers/gm_list_controller.rb
+++ app/controllers/gm_list_controller.rb
@@ -1,12 +1,49 @@
 class GmListController < ApplicationController
-  before_action :get_event
+  before_action :get_event, :get_unknown_user, :get_unknown_user_event
 
   def index
+
     # need to collect in the stuff. Make it easier to consume
     @game_masters = []
-    @event.user_events.each do |user_event|
-      @game_masters = (@game_masters << user_event.game_masters).flatten!
+
+    @event.sessions.each do |session|
+      session.tables.each do |table|
+        pad_gms table
+        @game_masters = (@game_masters << table.game_masters).flatten!
+      end
     end
-    @game_masters = @game_masters.sort { |a, b| a <=> b }
+
+    respond_to do |format|
+      format.html {
+        @game_masters = @game_masters.sort {|a, b| a <=> b}
+        render :index
+      }
+      format.json {render :index}
+    end
+  end
+
+  private
+  def pad_gms(table)
+    (1..table.gms_short)  .each do
+      add_unknown_gm table
+    end
+  end
+
+  def add_unknown_gm(table)
+    game_master            = GameMaster.new
+    game_master.table      = table
+    game_master.user_event = @user_event
+    table.game_masters << game_master
+  end
+
+  def get_unknown_user_event
+    @user_event       = UserEvent.new
+    @user_event.event = @event
+    @user_event.user  = @unknown
+  end
+
+  def get_unknown_user
+    @unknown      = User.new
+    @unknown.name = 'TBD'
   end
 end
---
+++ app/views/gm_list/index.json.jbuilder
@@ -0,0 +1,14 @@
+json.gm_list do
+  json.array! @game_masters do |gm|
+    json.set! :event_name, gm.table.session.event.name
+    json.set! :session_name, gm.table.session.name
+    json.set! :session_start, gm.table.session.start.localtime.to_formatted_s(:long)
+    json.set! :session_end, gm.table.session.end.localtime.to_formatted_s(:long)
+    json.set! :scenario, gm.table.scenario.long_name
+    json.set! :gm_name, gm.user_event.user.name
+    json.set! :gm_pfs_number, gm.user_event.user.pfs_number
+    json.set! :gm_email, gm.user_event.user.email
+    json.set! :gm_forum_username, gm.user_event.user.forum_username
+    json.set! :gm_title, gm.user_event.user.title
+  end
+end
