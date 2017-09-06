b17de422a9bb9cb8400d08c6bf130dcc731a8505
Jack Brown <brown285@umn.edu>
2017-09-06 13:28:24 -0500

    Add in receipts
    1) there is the all receipts, for a given convention
    2) there is an individual's receipts for all cons.
    3) from the registrations page, you can see each individual's receipt

---
+++ app/controllers/event_receipt_controller.rb
@@ -0,0 +1,8 @@
+class EventReceiptController < ApplicationController
+  before_action :get_event
+  before_action :get_user_event, only: [:show]
+
+  def index
+    render layout: "table-assignment"
+  end
+end
