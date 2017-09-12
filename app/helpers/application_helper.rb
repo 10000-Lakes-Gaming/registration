34762c920741f06347b17510deefbf605a96f830
Jack Brown <brown285@umn.edu>
2017-09-11 23:53:56 -0500

    Add sidebar listing for the additional payments pending.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,25 @@ module ApplicationHelper
   def yes_no (value)
     value ? "Yes" : "No"
   end
+
+  def receipts_exist?
+    current_user.user_events.any? {|user_event| user_event.total_price > 0}
+  end
+
+  def unpaid_payments?
+    current_user.user_events.any? {|user_event| user_event.additional_payments.any? {|payment| payment.unpaid?}}
+  end
+
+  def pending_payments
+    unpaid_payments = []
+    current_user.user_events.each do |user_event|
+      user_event.additional_payments.each do |payment|
+        if payment.unpaid?
+          unpaid_payments << payment
+        end
+      end
+    end
+    unpaid_payments
+  end
+
 end
