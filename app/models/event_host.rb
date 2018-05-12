5396e57c9e2d720a23da4b2303c49f7049d8bc6f
Jack Brown <brown285@umn.edu>
2018-05-12 15:03:30 -0500

    Update `EventHost.active?`
    Now checks start date
    Adds nil safety
    Can take in optional `date` param to test against
    Makes begin date required

--- app/models/event_host.rb
+++ app/models/event_host.rb
@@ -1,8 +1,28 @@
 class EventHost < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  validates :start_date, presence: true
 
-  def active?
-    end_date.to_date.future?
+  def end_date?
+    !end_date.nil?
+  end
+
+  def active? (date = nil)
+    if date.nil?
+      date = Date.today
+    end
+    date_start = start_date.to_date
+    if end_date?
+      date_end = end_date.to_date
+    else
+      # No end date, so always in the future.
+      date_end = Date.new(3000, 1, 1)
+    end
+    # Start date should never be nil, but never a bad thing to check
+    if date_start.nil?
+      date_start = Date.new(2000, 1, 1)
+    end
+    date.between?(date_start, date_end)
   end
 end
+
--- test/models/event_host_test.rb
+++ test/models/event_host_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class EventHostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @one    = event_hosts(:one)
+    @two    = event_hosts(:two)
+    @no_end = event_hosts(:no_end)
+  end
+
+  test "host one has end date" do
+    assert @one.end_date?
+  end
+
+  test "host no_end has no end date" do
+    assert_not @no_end.end_date?
+  end
+
+  test "host one is active!" do
+    assert @one.active?
+  end
+
+  test "host two is NOT active!" do
+    assert_not @two.active?
+  end
+
+  test "host no_end is NOT active!" do
+    assert @no_end.active?
+  end
+
+  test "host one active tomorrow" do
+    assert @one.active? Date.tomorrow
+  end
+
+  test "host two active yesterday" do
+    assert @two.active? Date.yesterday
+  end
+
+  test "host two not active 10 days ago" do
+    assert_not @two.active? Date.today - 10
+  end
+
+  test "no end active next year!" do
+    assert @no_end.active? Date.today + 365
+  end
+
 end
