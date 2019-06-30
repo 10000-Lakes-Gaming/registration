8b003c5b672fa6fc51f71f5b731868c704f409c0
Jack Brown <brown285@umn.edu>
2019-06-30 09:28:50 -0500

    Correct paizo_url field on csv

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -56,12 +56,12 @@ class Scenario < ActiveRecord::Base
   end
 
   def self.to_csv ()
-    attributes = %w{game_system, season, number, name, tier, description, author, type, hard_mode, pregen_only, retired, replayable, url}
+    attributes = %w{game_system season number name tier description author type hard_mode pregen_only retired replayable url}
 
     CSV.generate(headers: true) do |csv|
       csv << attributes
       all.each do |scenario|
-        csv << [scenario.game_system, scenario.season, scenario.scenario_number, scenario.name, scenario.tier, scenario.description, scenario.author, scenario.type_of, scenario.hard_mode, scenario.pregen_only, scenario.retired, scenario.evergreen, scenario.url]
+        csv << [scenario.game_system, scenario.season, scenario.scenario_number, scenario.name, scenario.tier, scenario.description, scenario.author, scenario.type_of, scenario.hard_mode, scenario.pregen_only, scenario.retired, scenario.evergreen, scenario.paizo_url]
       end
     end
   end
