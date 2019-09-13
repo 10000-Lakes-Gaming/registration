9c3df8626aecc77d262d67f3277afbf9e2130994
Jack Brown <brown285@umn.edu>
2019-09-13 18:14:37 -0500

    Add DCI Number as an alternative

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_214046) do
+ActiveRecord::Schema.define(version: 2019_09_13_225352) do
 
   create_table "additional_payments", force: :cascade do |t|
     t.string "category"
@@ -162,6 +162,7 @@ ActiveRecord::Schema.define(version: 2018_09_05_214046) do
     t.integer "gm_stars"
     t.boolean "venture_officer"
     t.string "title"
+    t.decimal "dci_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["pfs_number"], name: "index_users_on_pfs_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
