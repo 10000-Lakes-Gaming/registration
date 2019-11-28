c18a87f675efc2ace242816eef8767295cac274c
Jack Brown <brown285@umn.edu>
2019-11-27 23:47:03 -0600

    Add back in the rest of the code that got lost

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,13 +1,12 @@
 class AdminMailer < ApplicationMailer
 
   def cotn_gm_request_email(message)
-    @previous = message.email_list
-
+    message.subject = 'Call for Vols for MN-POP at Con of the North 2020' if message.subject.blank?
     # send an email to each individual
-    message.email_list.each do |email|
+    message.email_list.reject { |email| email.empty? }.each do |email|
       mail(subject: message.subject, to: email) do |format|
         @user = User.where(email: email).first
-        puts "Sending to #{user.full_name}"
+        puts "Sending to #{@user.formal_name}"
         format.html
         format.text
       end
