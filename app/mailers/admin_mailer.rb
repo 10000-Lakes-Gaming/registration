a2442adb1f962b5b3974221301d2fcf587bc2383
Jack Brown <brown285@umn.edu>
2019-11-27 22:03:04 -0600

    Change to send email per person

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,10 +1,17 @@
 class AdminMailer < ApplicationMailer
 
   def cotn_gm_request_email(message)
+    hardcoded = ['brown285@umn.edu']
+    @previous = message.email_list
 
-    mail(subject: message.subject, to:  ENV["GMAIL_SMTP_USERNAME"], bcc: message.email_list) do |format|
-      format.html
-      format.text
+    message.email_list = hardcoded
+
+    # send an email to each individual
+    message.email_list.each do |email|
+      mail(subject: message.subject, to: email) do |format|
+        format.html
+        format.text
+      end
     end
   end
 end
