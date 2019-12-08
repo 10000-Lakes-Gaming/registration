82f3eee407b0b468a3a94b755dc225fe2e0ea63f
Jack Brown <brown285@umn.edu>
2019-12-08 14:38:19 -0600

    Add default of false to opt_out
    Also made sure the cotn email was using it

---
+++ db/migrate/20191208202842_add_defaults_to_opt_out.rb
@@ -0,0 +1,11 @@
+class AddDefaultsToOptOut < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default(:users, :opt_out, from: nil, to: false)
+
+    users = User.where(opt_out: nil)
+    users.each do |user|
+      user.opt_out = false
+      user.save!
+    end
+  end
+end
