3fe33302bef5ab8543f7e85db51966b069cb093a
Jack Brown <brown285@umn.edu>
2020-10-07 20:25:46 -0500

    Adding totals

--- app/controllers/additional_payment_report_controller.rb
+++ app/controllers/additional_payment_report_controller.rb
@@ -6,10 +6,13 @@ class AdditionalPaymentReportController < ApplicationController
     return unless restrict_to_hosts
 
     @paid_payments = @event.additional_payments.reject { |payment| payment.unpaid? }
-    @paid_payments.sort_by! { |payment| [payment.user_event, payment.category, payment.created_at]}
+    @paid_payments.sort_by! { |payment| [payment.user_event, payment.category, payment.created_at] }
 
     @unpaid_payments = @event.additional_payments.select { |payment| payment.unpaid? }
-    @unpaid_payments.sort_by! { |payment| [payment.user_event, payment.category, payment.created_at]}
+    @unpaid_payments.sort_by! { |payment| [payment.user_event, payment.category, payment.created_at] }
+
+    @total_paid = @paid_payments.reduce(0) { |sum, payment| sum + payment.payment_amount }
+    @total_pending = @unpaid_payments.reduce(0) { |sum, payment| sum + payment.payment_price }
 
     render :'additional_payments/report'
   end
